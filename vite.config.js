ccb94755b6a31212c44d721f355a6c80bc3bc937
Shaun <skichenbrand@gmail.com>
2025-03-11 07:33:36 +0200

    enabled compiler

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines-per-function */
+
 import { resolve } from 'node:path';
 
 import react from '@vitejs/plugin-react';
@@ -105,7 +107,14 @@ export default defineConfig(async () => {
 		plugins: [
 			customCopyPlugin(),
 			libCss(),
-			react()
+			react({
+				babel: {
+					compact: true,
+					plugins: [
+						['babel-plugin-react-compiler', ReactCompilerConfig]
+					]
+				}
+			})
 		],
 		build: {
 			lib: {
