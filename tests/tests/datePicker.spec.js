a32b9d81ee8bc0f2ade2817cc23d5a13758b99b1
Shaun Kichenbrand <shaunkich@intenda.za.com>
2025-03-28 09:19:38 +0200

    test: fixed automated tests

--- tests/tests/datePicker.spec.js
+++ tests/tests/datePicker.spec.js
@@ -7,10 +7,17 @@ import '../helpers/setup';
 import { getRandomString } from '../helpers';
 import { testSteps } from '../helpers/awaitLocatorActions';
 
-test('aaa', async ({ page }) => {
-	await page.waitForFunction(() => {
+test('Date Picker', async ({ page }) => {
+	await testSteps([
+		'type , datePicker , #inputViewportValue',
+		'ss , valueOverride , 1985/11/26 , datePicker/w/2',
+		'gs , valueFormatted , 1985-11-26 , datePicker/w/2',
+		'chooseDatePickerDate , 1986/3/12 , datePicker/w/2'
+	]);
+
+	/*await page.waitForFunction(() => {
 		const elements = Array.from(document.querySelectorAll('.cpnLabel'));
 
 		return elements.length > 0 && elements.every(el => el.textContent.trim() === '<test>success');
-	}, { timeout: 30000 });
+	}, { timeout: 30000 });*/
 });
